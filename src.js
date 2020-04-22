2ef33ab83483efd2d3c1988cf040f700e4217c59
Vicky1526 <63657635+Vicky1526@users.noreply.github.com>
2020-04-22 15:10:45 +0200

    imgBig js

---
+++ src.js
@@ -0,0 +1,14 @@
+// id = "imgBig"
+// id = "photoTitle"
+// id = "photoDescription"
+let name = 'Vicky';
+console.log(`Hello, Vicky!`)
+
+
+var data = {
+    photo: "/img/20200421-madartavlat-tajkep-termeszet-kreativ-3573351.jpg",
+    title: "Odio pellentesque diam",
+    description: "Nunc mi ipsum faucibus vitae aliquet nec ullamcorper sit amet.Amet facilisis magna etiam tempor orci eu lobortis elementum.Vulputate enim nulla aliquet porttitor.Non arcu risus quis varius quam quisque id diam.Tristique senectus et netus et malesuada     fames ac.Arcu risus quis varius quam quisque id diam.Duis at tellus at urna condimentum.Fringilla phasellus faucibus scelerisque eleifend donec pretium vulputate sapien.Adipiscing diam donec adipiscing tristique risus nec feugiat. Fermentum posuere urna nec tincidunt praesent.Euismod elementum nisi quis eleifend quam adipiscing vitae.",
+};
+
+$('#imgBig').attr('src', data.photo)
